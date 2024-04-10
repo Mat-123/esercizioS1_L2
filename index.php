b93cb03ad8c2221cc65ee42b08e49df21e48ddd9
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-04-10 18:39:38 +0200

    fixato esercizio

--- index.php
+++ index.php
@@ -3,21 +3,31 @@
 echo '<pre>' . print_r($_POST, true) . '</pre>'; 
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $checkbox = $_POST['invalidCheck'];
+    $checkbox = $_POST['check'];
 
     $errors = [];
 
-    if ($errors == []) {
-        header('Location: /esercizioS1_L2/success.php');
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Email non valida';
     }
+
+if (!filter_var($invalidCheck, FILTER_VALIDATE_BOOL)) {
+    $errors['check'] = 'You must agree before submitting.';
 }
 
-?>
+    if (strlen($password) < 8) {
+        $errors['password'] = 'Password troppo corta';
+    }
+
+    if ($errors == []) {
+    header('Location: /esercizioS1_L2/success.php');
+    }
+}?>
 
 
 <!DOCTYPE html>
@@ -26,24 +36,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
+
     <title>Form Validation</title>
 </head>
 <body>
+    <form action="" method="post" class="needs-validation" novalidate>
 <div class="container">
 <div class="row">
     <div class="col-8 mx-auto mt-4">
     <div class="mb-3">
     
-        <form action="" method="post" novalidate><div class="row">
+        <div class="row">
   <div class="col">
     
   <label for="name" class="form-label">Name</label>
-    <input type="text" class="form-control" placeholder="First name" id="name" aria-label="First name">
+    <input type="text" class="form-control" placeholder="First name" name="name" id="name" aria-label="First name" required>
   </div>
   <div class="col">
   <label for="surname" class="form-label">Surname</label>
-    <input type="text" class="form-control" placeholder="Surname" id="surname" aria-label="Last name">
+    <input type="text" class="form-control" placeholder="Surname" name="surname id="surname" aria-label="Last name" required>
   </div>
 </div>
 </div>
@@ -51,23 +62,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div class="row">
   <div class="col-8">
   <label for="email" class="form-label">Email</label>
-  <input type="email" class="form-control mb-3" id="email" placeholder="example@email.com">
+  <input type="email" class="form-control mb-3" id="email" name="email" placeholder="example@email.com" required>
   <label for="password" class="form-label">Password</label>
-  <input type="password" class="form-control mb-3" id="password" placeholder="example@email.com">
-  <input class="form-check-input" type="checkbox" value="" id="invalidCheck">
+  <input type="password" class="form-control mb-3" id="password" name="password" placeholder="example@email.com" required>
+  <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="invalidCheck" name="check" required>
       <label class="form-check-label" for="invalidCheck">
         Agree to terms and conditions
       </label>
       <div class="invalid-feedback">
         You must agree before submitting.
-      </div>
+      </div></div> 
       <div class="col-12">
   <button type="submit" class="btn btn-primary mt-2">Send</button></div>
-</form>
+
 </div>
 </div>
 </div>
     </div>
-</div>
+</div></form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
